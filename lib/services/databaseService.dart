1b6171b94ca2c38dc60bc2f75e49c62f01c6be7c
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-04-04 15:46:19 +0530

    Adding insert and getAllCoordinate methods

--- lib/services/databaseService.dart
+++ lib/services/databaseService.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 
+import 'package:falcon_corona_app/models/coordinate.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DatabaseService {
-	dynamic initDatabase() async {
+	Future<Database> initDatabase() async {
 		final Future<Database> database=openDatabase(
 				join(await getDatabasesPath(), 'coordinate_database.db'),
 				onCreate: (db, version) {
@@ -15,5 +16,15 @@ class DatabaseService {
 				version: 1,
 		);
 		return database;
-	} 
+	}
+
+	Future<void> insertCoordinate(Database database, Coordinate coordinate) async {
+		await database.insert(
+				'coordinate_database',
+				coordinate.toJson(),
+				conflictAlgorithm: ConflictAlgorithm.replace,
+		);
+	}
+
+	Future<List<Coordinate>> getAllCoordinates(Database database) async {}
 }
